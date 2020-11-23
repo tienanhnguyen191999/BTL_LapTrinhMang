989289a8fa83d86da309cba732356c4c26fc1cb8
anhnt <tienanhnguyen191999@gmail.com>
2020-11-23 23:28:08 +0700

    #10 add dynamic direction when ball intersect with bar

---
+++ src/gameplay/develop/test.java
@@ -0,0 +1,20 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package gameplay.develop;
+
+import java.util.ArrayList;
+import java.util.Random;
+import jdk.internal.util.xml.impl.Pair;
+
+/**
+ *
+ * @author tienanh
+ */
+public class test {
+	public static void main(String[] args) {
+		
+	}
+}
